4e6cadfe6106430f38cd2a8ae0c797ec4ed9d381
Cd-Vishal007 <vishal.codedrill@gmail.com>
2021-05-21 13:18:00 +0530

    changes

---
+++ GraphqlWithNodeAndMongoDb/backend/config/database.js
@@ -0,0 +1,16 @@
+// The file server/config/database.js
+// #1 Import mongoose
+const mongoose = require('mongoose');
+
+// #2 Create a query string to connect to MongoDB server
+const DB_URI = 'mongodb://localhost:27017/graphql-app';
+
+// #3 Connect to MongoDB
+mongoose.connect(DB_URI, { useNewUrlParser: true });
+
+// #4 Add basic event listeners on the mongoose.connection object
+mongoose.connection.once('open', () => console.log('Connected to a MongoDB instance'));
+mongoose.connection.on('error', error => console.error(error));
+
+// #5 Export mongoose. You’ll use it in server/server.js file
+module.exports = mongoose;
